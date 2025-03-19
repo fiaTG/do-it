d046eb892d76db21a32f85725a0eadcd23381996
fiaTG <giese.fia@gmail.com>
2025-03-19 08:18:08 +0100

    Projektstruktur angelegt
    Signed-off-by: fiaTG <giese.fia@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# do-it
-
+17.03.2025  --- php.info() erstellt --- Serverkonfiguration überprüft
+18.03.2025  ---php.info() gelöscht
+18.03.2025  ---Projektstruktur erstellt.
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koala SCSS Test</title>
+    <link rel="stylesheet" href="styles.css"> <!-- SCSS-kompiliertes CSS -->
+</head>
+<body>
+
+    <h1>Koala SCSS Test</h1>
+    <p>Wenn alles funktioniert, sollte der Hintergrund blau sein.</p>
+
+</body>
+</html>
